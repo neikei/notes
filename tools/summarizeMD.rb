b1997ef8ed311eb370e3598e91d0cc3bad53aac8
neikei <niklas@vosskoetter.org>
2017-08-10 17:34:12 +0200

    Improved anchor link creation

--- tools/summarizeMD.rb
+++ tools/summarizeMD.rb
@@ -7,55 +7,54 @@ require 'optparse/time'
 require 'ostruct'
 require 'yaml'
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
 class MDUtils
 
 
-	def self.summarize(args)
+    def self.summarize(args)
 
-		filename = args[0]
-		options  = args[1]
+        filename = args[0]
+        options  = args[1]
 
-		if !File.exist?(filename)
-			abort("Please enter a valid MD file.")
-		end
+        if !File.exist?(filename)
+            abort("Please enter a valid MD file.")
+        end
 
-		puts "Generating summary for file " + filename
+        puts "Generating summary for file " + filename
 
-		# In order to be sure to not write on existing file
-		temp_file = Tempfile.new("summarizeMd")
+        # In order to be sure to not write on existing file
+        temp_file = Tempfile.new("summarizeMd")
 
-		summary = generate_sumary_heading(options)
-		forbidden_words = ['Table of contents', 'define', 'pragma', '#!', '# Desc:']
+        summary = generate_sumary_heading(options)
+        forbidden_words = ['Table of contents', 'define', 'pragma', '#!', '# Desc:']
 
-		File.open(filename, 'r') do |f|
-			if options.verbose 
-				puts "Generate summary for section(s): "
-			end
-			f.each_line do |line|
-		    	if !(!line.start_with?("#") || forbidden_words.any? { |w| line =~ /#{w}/ }) 
+        File.open(filename, 'r') do |f|
+            if options.verbose 
+                puts "Generate summary for section(s): "
+            end
+            f.each_line do |line|
+                if !(!line.start_with?("#") || forbidden_words.any? { |w| line =~ /#{w}/ }) 
                     title = line.gsub("#", "").strip
                     # Create meaningful anchor links
-                    date = line.delete('#:~/.()')
-                    date = date.lstrip
-                    date = date.gsub(" ", "-").strip
-                    href = "#{date}"
-                    # href = "section-id-#{$.}" # old href creation
-				    anchor = "<div id='#{href}'/>\n\n"
-				    temp_file.puts anchor + line
-				    summary += "  " * (line.count("#")-1) + "- [#{title}](\##{href})" + "\n"
-				    if options.verbose 
-				    	puts "\t- [#{title}]"
-				    end
-			 	else
-			 		temp_file.puts line
-			 	end   
-		   	end
+                    href = line.delete('#:~/.()')    # Remove special characters
+                    href = href.lstrip               # Remove leading whitespaces
+                    href = href.gsub(" ", "-").strip # Replace whitespaces with minus
+                    href = "#{href}"                 # Add hashmark as leading character
+                    anchor = "<div id='#{href}'/>\n\n"
+                    temp_file.puts anchor + line
+                    summary += "  " * (line.count("#")-1) + "- [#{title}](\##{href})" + "\n"
+                    if options.verbose 
+                        puts "\t- [#{title}]"
+                    end
+                 else
+                     temp_file.puts line
+                 end   
+               end
                         temp_file.close
-		end
+        end
                 summary += "  \n\n"
-
+        
                 with_output_file(options, filename) do |output_file|
                     if (options.replace_summary_token.empty?)
                         prepend_to_file(summary, temp_file, output_file) 
@@ -66,7 +65,7 @@ class MDUtils
                 
                 temp_file.unlink
                 puts "Done.\n"
-	end #(end summarize)
+    end #(end summarize)
         
         def self.prepend_to_file(toPrepend, source_file, target_file)
             File.open(target_file, 'w') do |fo|
@@ -160,12 +159,12 @@ class OptparseSummarize
       end
       
       opts.on("-t", "--replace_summary_token [summary_token]",
-              "Replace the line containing the supplied token, with the sumrmary, instead of putting the summary in the top of the file. If the token is not found, then no summary is inserted.") do |out|
+              "Replace the line containing the supplied token, with the summary, instead of putting the summary in the top of the file. If the token is not found, then no summary is inserted.") do |out|
         options.replace_summary_token << out
       end
       
       opts.on("-l", "--heading_level [heading level]", Integer,
-              "Specify the heading level, eg. The number of ## to insert before the Sumary heading. Set this to 0 to not add a heading to the summary.") do |l|
+              "Specify the heading level, eg. The number of ## to insert before the Summary heading. Set this to 0 to not add a heading to the summary.") do |l|
         options.heading_level = l
       end
 
